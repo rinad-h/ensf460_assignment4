7e5973057f23311040383946706119de6a9bb0db
Rinad Hamid <rinad.hamid@ucalgary.ca>
2025-11-22 20:03:58 -0700

    new ios.c for pb1 functionality for project that works
    Refactor IOs.c to include timer and interrupt handling.

--- IOs.c
+++ IOs.c
@@ -9,126 +9,126 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include "clkChange.h"
 #include "UART2.h"
 #include "TimeDelay.h"
 #include "ADC.h"
 #include "IOs.h"
 
-// Shared flags and variables from other modules
-extern volatile uint8_t pb1_event;
-extern volatile uint8_t timer_flag;
-extern volatile uint8_t mode_changed;
-extern volatile uint16_t sleep_flag;
+#pragma config BWRP = OFF
+#pragma config BSS = OFF
+#pragma config GWRP = OFF
+#pragma config GCP = OFF
+#pragma config FNOSC = FRC
+#pragma config IESO = OFF
+#pragma config POSCMOD = NONE
+#pragma config OSCIOFNC = ON
+#pragma config POSCFREQ = HS
+#pragma config SOSCSEL = SOSCHP
+#pragma config FCKSM = CSECMD
+#pragma config WDTPS = PS32768
+#pragma config FWPSA = PR128
+#pragma config WINDIS = OFF
+#pragma config FWDTEN = OFF
+#pragma config BOREN = BOR3
+#pragma config PWRTEN = ON
+#pragma config I2C1SEL = PRI
+#pragma config BORV = V18
+#pragma config MCLRE = ON
+#pragma config ICS = PGx2
+#pragma config DSWDTPS = DSWDTPSF
+#pragma config DSWDTOSC = LPRC
+#pragma config RTCOSC = SOSC
+#pragma config DSBOREN = ON
+#pragma config DSWDTEN = ON
 
+volatile uint8_t system_state = 0;  // 0 = OFF, 1 = ON
+volatile uint8_t active_led = 1;     // 1 = LED1, 2 = LED2
+volatile uint8_t pb1_event = 0;
+volatile uint8_t pb1_long_press = 0;
+volatile uint8_t timer_flag = 0;
+volatile uint16_t sleep_flag = 0;
 
-//Draws a live bar graph based on ADC value
-void display_bargraph(uint16_t adc_value) {
-    char line[80];
-    const int BAR_WIDTH = 32;
-    int num_stars = (int)(((long)adc_value * BAR_WIDTH) / 1023);
+extern void handle_pwm_interrupt(void);
+void setup_timer1(void);
 
-    if (num_stars < 0) num_stars = 0;
-    if (num_stars > BAR_WIDTH) num_stars = BAR_WIDTH;
+void setup_timer1(void) {
+    T1CONbits.TON = 0;
+    T1CONbits.TCS = 0;
+    T1CONbits.TCKPS = 0b01;  // 1:8 prescaler
+    TMR1 = 0;
+    PR1 = 62499;  // ~500ms interrupt at 500kHz clock
+    IFS0bits.T1IF = 0;
+    IEC0bits.T1IE = 1;
+    T1CONbits.TON = 1;
+}
 
-    int pos = 0;
-    line[pos++] = '\r';
-
-    const char *prefix = "Mode 0: ";
-    strcpy(&line[pos], prefix);
-    pos += strlen(prefix);
-
-    // Add stars for the filled portion
-    for (int i = 0; i < num_stars && pos < sizeof(line) - 1; i++)
-        line[pos++] = '*';
+int main(void) {
+    newClk(500);
     
-    // Add spaces for the remaining portion
-    for (int i = num_stars; i < BAR_WIDTH && pos < sizeof(line) - 1; i++)
-        line[pos++] = ' ';
-
-    // Show ADC reading at the end
-    pos += snprintf(&line[pos], sizeof(line) - pos, " 0x%03X", adc_value);
-    line[pos] = '\0';
-
-    Disp2String(line);
+    AD1PCFG = 0xFFFF;
+    AD1PCFGbits.PCFG12 = 0;  // Enable AN12 for ADC
+    
+    IOinit();
+    InitUART2();
+    init_ADC();
+    setup_timer1();
+    
+    delay_ms(10);
+    
+    Disp2String("\r\n*** LED Control System ***\r\n");
+    Disp2String("Short press PB1: Toggle ON/OFF\r\n");
+    Disp2String("Long press PB1 (when ON): Swap LEDs\r\n");
+    Disp2String("*** SYSTEM OFF ***\r\n");
+    
+    while (1) {
+        IOCheck();
+        delay_ms(10);
+    }
+    
+    return 0;
 }
 
-// Initialize input/output pins and enable CN interrupts for PB1
-void IOinit(void) {
-    TRISBbits.TRISB7 = 1;          // Set RB7 (PB1) as input
-    CNPU2bits.CN23PUE = 1;         // Enable internal pull-up
-    CNEN2bits.CN23IE = 1;          
-    IPC4bits.CNIP = 6;             
-    IFS1bits.CNIF = 0;             
-    IEC1bits.CNIE = 1;             
+void __attribute__((interrupt, no_auto_psv)) _T1Interrupt(void) {
+    IFS0bits.T1IF = 0;
+    timer_flag = 1;
 }
 
-//Handles all mode logic and user interactions
-void IOCheck(void) {
-    static uint8_t stream_started = 0;
-    uint16_t adc_value;
-
-    // Check if PB1 was pressed
-    if (pb1_event) {
-        pb1_event = 0;
-
-        if (current_mode == MODE_0_BARGRAPH) {
-            current_mode = MODE_1_STREAM;
-            stream_started = 0;
-            mode_changed = 1;
+void __attribute__((interrupt, no_auto_psv)) _CNInterrupt(void) {
+    static uint8_t button_was_pressed = 0;
+    static uint16_t press_start_time = 0;
+    static uint16_t main_loop_counter = 0;
+    uint8_t current_state = PORTBbits.RB7;
+    
+    main_loop_counter++;  // Rough time counter (increments each CN event)
+    
+    if (button_was_pressed == 1 && current_state == 1) {
+        // Button released
+        uint16_t press_duration = main_loop_counter - press_start_time;
+        
+        if (press_duration > 200) {
+            pb1_long_press = 1;
         } else {
-            if (!stream_started) {
-                stream_started = 1;
-                Disp2String("STREAMING_START\n");
-                delay_ms(100);
-            } else {
-                stream_started = 0;
-                Disp2String("STREAMING_STOP\n");
-                current_mode = MODE_0_BARGRAPH;
-                mode_changed = 1;
-            }
+            pb1_event = 1;
         }
+        
+        button_was_pressed = 0;
+    } else if (current_state == 0) {
+        // Button pressed
+        button_was_pressed = 1;
+        press_start_time = main_loop_counter;
     }
+    
+    IFS1bits.CNIF = 0;
+}
 
-    //Handle transitions between modes
-    if (mode_changed) {
-        mode_changed = 0;
+void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void) {
+    IFS0bits.T2IF = 0;
+    T2CONbits.TON = 0;
+    sleep_flag = 1;
+}
 
-        if (current_mode == MODE_0_BARGRAPH) {
-            Disp2String("\r\n\n*** MODE 0: Bar Graph Display ***\r\n");
-        } else {
-            Disp2String("\r\n\n*** MODE 1: Data Streaming ***\r\n");
-            Disp2String("Press PB1 to START data stream...\r\n");
-        }
-    }
-
-    //Mode 0: Display live ADC value as bar graph
-    if (current_mode == MODE_0_BARGRAPH) {
-        if (timer_flag) {
-            timer_flag = 0;
-            adc_value = do_ADC();
-            display_bargraph(adc_value);
-        }
-    }
-
-    //Mode 1: Stream ADC values continuously
-    else if (current_mode == MODE_1_STREAM && stream_started) {
-        while (stream_started && current_mode == MODE_1_STREAM) {
-            adc_value = do_ADC();
-            char buf[20];
-            sprintf(buf, "%u\n", adc_value);
-            Disp2String(buf);
-
-            delay_ms(20);
-
-            // Stop stream if button pressed again
-            if (pb1_event) {
-                pb1_event = 0;
-                stream_started = 0;
-                Disp2String("STREAMING_STOP\n");
-                current_mode = MODE_0_BARGRAPH;
-                mode_changed = 1;
-                break;
-            }
-        }
-    }
+void __attribute__((interrupt, no_auto_psv)) _T3Interrupt(void) {
+    IFS0bits.T3IF = 0;
+    handle_pwm_interrupt();  // Call PWM handler from IOs.c
 }
