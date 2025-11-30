baace999f7194bf0751f43d1ee3a852a3f73492c
Rinad Hamid <rinad.hamid@ucalgary.ca>
2025-11-29 20:30:27 -0700

    Update IOs.h with LED and button event definitions
    Added new constants and external variables for LED control and button events.

--- IOs.h
+++ IOs.h
@@ -1,26 +1,35 @@
-/*
- * File:   IOs.h
- * Author: Rinad Hamid, Simar Kandola, Abia Jahangir
-
- */
-
 #ifndef IOS_H
 #define IOS_H
 
 #include <xc.h>
+#include <p24F16KA101.h>
 #include <stdint.h>
 
-// --- External global variables from ios.c ---
-extern volatile uint8_t pb1_event;
-extern volatile uint8_t pb1_long_press;
-extern volatile uint8_t timer_flag;
-extern volatile uint8_t system_state;    // 0 = OFF, 1 = ON
-extern volatile uint8_t active_led;      // 1 = LED1, 2 = LED2
+#define LED1 LATBbits.LATB9
+#define LED2 LATAbits.LATA6
+#define PWM_PERIOD 45
+#define LONG_PRESS_THRESHOLD 600
 
-// --- Function prototypes ---
+extern volatile uint8_t system_state;
+extern volatile uint8_t active_led;
+extern volatile uint8_t uart_transmit_mode;
+extern volatile uint32_t millisecond_counter;
+extern volatile uint8_t timer_flag;
+extern volatile uint16_t pwm_duty_cycle;
+extern volatile uint8_t blink_mode;
+extern volatile uint16_t blink_counter;
+extern volatile uint8_t blink_state;
+extern volatile uint16_t current_adc_value;
+
+extern volatile uint8_t pb1_release_event;
+extern volatile uint16_t pb1_duration_timer;
+extern volatile uint8_t pb2_release_event;
+extern volatile uint8_t pb3_release_event;
+
+uint8_t get_current_intensity_percent(void);
+void update_status_line(void);
+void set_led_intensity(uint16_t adc_value);
 void IOinit(void);
 void IOCheck(void);
-void set_led_intensity(uint16_t adc_value);
-void handle_pwm_interrupt(void);
 
-#endif  // IOS_H
+#endif
